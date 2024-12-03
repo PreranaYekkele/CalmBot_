c3996ae620072e8ce8d3995a47dffc9ad7791299
Prerana Yekkele <preranayekkele@gmail.com>
2024-12-03 17:02:45 +0530

    Delete CalmBot directory

--- CalmBot/config.py
+++
@@ -1,5 +0,0 @@
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'calm_bot'  # or whatever username you chose
-MYSQL_PASSWORD = 'calmbot'
-MYSQL_PORT = 3307    # the new port you selected
-MYSQL_DB = 'calmbot'
